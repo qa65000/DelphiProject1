0de4a3fc9da0c0691d99fcd96d4af8a9a9939560
Yamasho3 <qa65000@gmail.com>
2021-02-18 20:21:54 +0900

    コメント変更

--- Unit1.pas
+++ Unit1.pas
@@ -27,9 +27,9 @@ implementation
 
 procedure TForm1.Button1Click(Sender: TObject);
 begin
-       Label1.Tag  := Label1.Tag +1;                    { 押し数を+1する }
-//      Label1.Text := '押し回数'+Label1.Tag.ToString;  { 押し数を表示する }
-      Label1.Text := '押し回数'+IntToStr(Label1.Tag);  { 押し数を表示する }
+      Label1.Tag  := Label1.Tag +1;                    { 押し数を+1する }
+      Label1.Text := '押し回数'+Label1.Tag.ToString;    // "押し回数:"文字列と数値->文字列変換した値　文字列として代入する
+(*   Label1.Text := '押し回数'+IntToStr(Label1.Tag);   // "押し回数:"文字列と数値->文字列変換した値　文字列として代入する  *)
 end;
 
 end.
